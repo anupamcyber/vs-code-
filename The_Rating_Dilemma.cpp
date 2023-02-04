1e03ac1fd31428fbc863a43d5e289500e7b24e6f
anupamcyber <anupanshaw@gmail.com>
2023-02-04 15:31:59 +0530

    file modied ratinf delima

--- The_Rating_Dilemma.cpp
+++ The_Rating_Dilemma.cpp
@@ -1,19 +1,6 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    int t;
-    cin >> t;
-    while(t--)
-    {
-        int s;
-        cin >> s;
-        int x = 1, y = -1;
-        while (x+y<s)
-        {
-            x += 1;
-        }
-        cout << x * y << endl;
-    }
-    return 0;
+    cout << "we have changed"
 }
